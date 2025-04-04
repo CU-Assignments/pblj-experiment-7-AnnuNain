6f6468eab73ff9222ea64a6b10dbc2aff7690bba
AnnuNain <145455659+AnnuNain@users.noreply.github.com>
2025-04-04 23:39:17 +0530

    Create Student.java

---
+++ StudentManagement/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    private int id;
+    private String name;
+    private String department;
+    private double marks;
+
+    public Student(int id, String name, String department, double marks) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.marks = marks;
+    }
+
+    // Getters & Setters
+}
